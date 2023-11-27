76f6f3eae2321dafe4ffe86551b8d81b039b4719
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2023-11-27 17:06:13 +0600

    automate build path

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(express.urlencoded({ extended: true }))
 const planetsRouter = require('./routes/planets/planets.router')
 app.use(planetsRouter)
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 module.exports = app
