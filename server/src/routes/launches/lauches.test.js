2868c37ff30083e1a22c4016bdae5d0b627f40d6
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2023-11-28 16:43:35 +0600

    running test cases with jest and supertest

---
+++ server/src/routes/launches/lauches.test.js
@@ -0,0 +1,62 @@
+const app = require("../../app")
+const request = require('supertest')
+
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async () => {
+        const response = await request(app)
+            .get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200)
+    })
+})
+
+describe('Test POST /launches', () => {
+    const completeLaunchData = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'January 4, 2028'
+    }
+    const launchDataWithoutDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+    }
+
+    test('It should respond with 201 created', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(completeLaunchData)
+            .expect('Content-Type', /json/)
+            .expect(201)
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf()
+        const responseDate = new Date(response.body.launchDate).valueOf()
+        expect(responseDate).toBe(requestDate)
+        expect(response.body).toMatchObject(launchDataWithoutDate)
+    })
+    test('It should catch missing required properties', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(launchDataWithoutDate)
+            .expect('Content-Type', /json/)
+            .expect(400)
+        expect(response.body).toStrictEqual({
+            error: 'Missing required launch property'
+        })
+    })
+    test('It should catch invalid dates', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send({
+                mission: 'USS Enterprise',
+                rocket: 'NCC 1701-D',
+                target: 'Kepler-186 f',
+                launchDate: 'zoot'
+            })
+            .expect('Content-Type', /json/)
+            .expect(400)
+        expect(response.body).toStrictEqual({
+            error: 'Invalid launch date'
+        })
+    })
+})
