2426728e5f8af21aee519759c5eefb6f85469071
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2023-12-28 18:00:14 +0600

    .gitignore

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const PORT = process.env.PORT || 8000
 
 const http = require('http')
+require('dotenv').config()
 const app = require('./app')
 const { mongoConnect } = require('./services/mongo')
 const { loadPlanetsData } = require('./models/planets.model')
